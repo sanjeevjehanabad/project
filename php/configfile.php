7bbbbb3f4c0ead12b9dd64eda8bcb35a6921e897
sanjeevjehanabad <sanjeevjehanabad@gmail.com>
2013-12-29 04:13:21 +0530

    home

--- php/configfile.php
+++
@@ -1,6 +0,0 @@
-<?php
-$consumer_key='275695418855-ms7conjjajgfhs2fq2u7h02nr8s98qto.apps.googleusercontent.com';
-$consumer_secret='lpYwHYGmz8_Pw3erVQslGd_n';
-$callback='http://localhost/contact.php';
-$emails_count='500'; // max-results
-?>
