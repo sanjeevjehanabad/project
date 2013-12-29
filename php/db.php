26dbc40c3b4cdfdac63c9a6ed23f0b1cbbfddd38
sanjeevjehanabad <sanjeevjehanabad@gmail.com>
2013-12-29 12:26:44 +0530

    5

--- php/db.php
+++ php/db.php
@@ -3,6 +3,15 @@ $mysql_hostname = "mysql://127.5.212.2:3306/";
 $mysql_user = "adminDH7Hs5I";
 $mysql_password = "tED15rM1fQld";
 $mysql_database = "sanjeevjehanabad";
-$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
-mysql_select_db($mysql_database, $bd) or die("Could not select database");
+
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+
+$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+$dbh = new PDO($dsn, DB_USER, DB_PASS) or die("Could not connect database");
+mysql_select_db($mysql_database, $dbh) or die("Could not select database");
+
 ?>
