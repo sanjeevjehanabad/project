01ae65241a0c7aed53c9e92e20b378a3a887b45b
sanjeevjehanabad <sanjeevjehanabad@gmail.com>
2013-12-29 23:02:24 +0530

    last7

--- php/contact.php
+++ php/contact.php
@@ -13,7 +13,7 @@ $access_token_secret=$oauth->rfc3986_decode($contact_access['oauth_token_secret'
 $contacts= $getcontact_access->GetContacts($oauth, $access_token, $access_token_secret, false, true,$emails_count);
 
 //Email Contacts
-foreach($contacts as $k => $a)
+/*foreach($contacts as $k => $a)
 {
 $final = end($contacts[$k]);
 $phone1 = end($a['gd$phoneNumber'][0]);
@@ -22,9 +22,25 @@ foreach($final as $email)
 echo "<div style='background-color:#CCCCCC'>";
 echo $email['address'] .$phone1."<br />";
 echo "</div>";
-}
-}?>
+}*/
+/*
+  echo "<pre>";                //print array to console
+    {print_r($contacts);}
+  echo "</pre>";
+  */
+$length = count($contacts);
+for ($i = 0; $i < $length; $i++) {
+  echo "<pre>";                //print array to console
+    {
+	print_r(end($contacts[$i]['title']));
+	echo "  ";
+	print_r($contacts[$i]['gd$email'][0]['address']);
+	echo "  ";
+	print_r(end($contacts[$i]['gd$phoneNumber'][0]));}
+  echo "</pre>";
+}  
 
+?>
 <?php
-print "<a class='home' href='home.php'>Home</a>";
+print "<a class='logout' href='home.php'>Home</a>";
 ?>
